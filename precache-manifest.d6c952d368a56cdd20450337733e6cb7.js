768471e0eb777b026fc1cc82bbfe995cd90e2734
root <okwuosahpaschal@gmail.com>
2020-03-31 07:45:28 +0000

    Updates

---
+++ precache-manifest.d6c952d368a56cdd20450337733e6cb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db7280cc359db9422ab0f7197916ac8",
+    "url": "/Movie-database/index.html"
+  },
+  {
+    "revision": "52eca88e80351a12a683",
+    "url": "/Movie-database/static/css/main.0224b6dc.chunk.css"
+  },
+  {
+    "revision": "d59f26af496ad3459873",
+    "url": "/Movie-database/static/js/2.bd97b013.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Movie-database/static/js/2.bd97b013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52eca88e80351a12a683",
+    "url": "/Movie-database/static/js/main.8e08a5e1.chunk.js"
+  },
+  {
+    "revision": "c538bfabbafeb3b88ac1",
+    "url": "/Movie-database/static/js/runtime-main.6a14a205.js"
+  }
+]);
